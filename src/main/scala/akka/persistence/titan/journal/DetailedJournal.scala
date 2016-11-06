74b633f920c789f1387e0d9fea2c2d4ebf62f656
Adrian Florea <acflorea@gmail.com>
2016-11-06 17:04:13 +0100

    Flatten properties map - WIP

--- src/main/scala/akka/persistence/titan/journal/DetailedJournal.scala
+++ src/main/scala/akka/persistence/titan/journal/DetailedJournal.scala
@@ -4,6 +4,7 @@ import akka.actor.{Actor, ActorLogging}
 import akka.persistence.PersistentRepr
 import akka.persistence.titan.TitanCommons._
 import org.apache.tinkerpop.gremlin.structure.Graph
+import scala.collection.JavaConverters._
 
 /**
   * Actor in charge with storing Journal events details
@@ -29,7 +30,9 @@ class DetailedJournal(config: TitanJournalConfig) extends Actor with ActorLoggin
           entry._2 match {
             // Flatten nested structures
             case nestedMap: java.util.Map[String, Any] =>
-              ""
+              _flatten(entry._1, nestedMap) foreach { nested_entry =>
+                detailsVertex.property(s"$PAYLOAD_KEY.${nested_entry._1}", nested_entry._2)
+              }
             case _ =>
               detailsVertex.property(s"$PAYLOAD_KEY.${entry._1}", entry._2)
           }
@@ -42,6 +45,20 @@ class DetailedJournal(config: TitanJournalConfig) extends Actor with ActorLoggin
         log.warning(s"Unable to retrieve parent vertex for payload $payload and parentId $parentId")
       }
 
+  }
+
+
+  def _flatten(key: String, map: java.util.Map[String, Any]): scala.collection.mutable.Map[String, Any] = {
+    val flatRepr = map.asScala map { entry =>
+      entry._2 match {
+        // Flatten nested structures
+        case nestedMap: java.util.Map[String, Any] =>
+          _flatten(entry._1, nestedMap)
+        case _ =>
+          s"$key.${entry._1}" -> entry._2
+      }
+    }
+    flatRepr.flatten
   }
 
 
